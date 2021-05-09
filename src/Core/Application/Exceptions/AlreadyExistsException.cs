50418d0c2c9e8349c4a910157be6d763735eb11d
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-05-09 18:35:29 +0200

    Add exceptions

---
+++ src/Core/Application/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Scrumboard.Application.Exceptions
+{
+    public class AlreadyExistsException : Exception
+    {
+        public AlreadyExistsException() 
+            : base() { }
+
+        public AlreadyExistsException(string message) 
+            : base(message) { }
+
+        public AlreadyExistsException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        public AlreadyExistsException(string name, object key)
+            : base($"{name} ({key}) already exist.") { }
+    }
+}
---
+++ src/Core/Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Scrumboard.Application.Exceptions
+{
+    public class BadRequestException : ApplicationException
+    {
+        public BadRequestException(string message) 
+            : base(message) { }
+    }
+}

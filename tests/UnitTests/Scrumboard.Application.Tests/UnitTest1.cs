09f252546d2e5224f81668f95aabc1dee1d3ae0b
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-05-13 15:30:25 +0200

    Add Application.Test project

---
+++ tests/UnitTests/Scrumboard.Application.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Scrumboard.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

909de4889f43d33036e981c41914d7befc7c0239
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-05-16 18:27:37 +0200

    Add queries for boards

---
+++ src/Core/Application/Features/Boards/Queries/GetBoardDetail/GetBoardDetailQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Scrumboard.Application.Dto;
+using System;
+
+namespace Scrumboard.Application.Features.Boards.Queries.GetBoardDetail
+{
+    public class GetBoardDetailQuery : IRequest<BoardDetailDto>
+    {
+        public Guid BoardId { get; set; }
+    }
+}

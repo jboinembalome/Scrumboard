ec43a8dfa13d3ac6e3838686cbb4e9e37731b398
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-05-16 18:26:57 +0200

    Add specifications for boards queries

---
+++ src/Core/Application/Features/Boards/Specifications/BoardWithAllSpec.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.Specification;
+using Scrumboard.Domain.Entities;
+using System;
+
+namespace Scrumboard.Application.Features.Boards.Specifications
+{
+    public class BoardWithAllSpec : Specification<Board>, ISingleResultSpecification
+    {
+        public BoardWithAllSpec(Guid boardId)
+        {
+            Query.Where(b => b.Id == boardId);
+            Query.Include(b => b.Labels);
+
+            Query.Include(b => b.ListBoards)
+                    .ThenInclude(l => l.Cards)
+                    .ThenInclude(c => c.Checklists)
+                    .ThenInclude(c => c.ChecklistItems);
+
+            Query.Include(b => b.ListBoards)
+                    .ThenInclude(l => l.Cards)
+                    .ThenInclude(c => c.Labels);
+
+            Query.Include(b => b.ListBoards)
+                    .ThenInclude(l => l.Cards)
+                    .ThenInclude(c => c.Comments);
+
+            Query.Include(b => b.ListBoards)
+                   .ThenInclude(l => l.Cards)
+                   .ThenInclude(c => c.Attachments);
+        }
+    }
+}

b3f13a54fc92a43d9ed6fdfb9c008e4c2b788d53
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-05-23 19:12:38 +0200

    Add database configuration

---
+++ tests/IntegrationTests/Scrumboard.Application.IntegrationTests/Persistence/DatabaseFixture.cs
@@ -0,0 +1,93 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Moq;
+using Respawn;
+using Scrumboard.Application.Interfaces.Common;
+using Scrumboard.Application.Interfaces.Persistence;
+using Scrumboard.Infrastructure.Persistence;
+using Scrumboard.Infrastructure.Persistence.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace Scrumboard.Application.IntegrationTests.Persistence
+{
+    public class DatabaseFixture
+    {
+        private const string DEFAULT_SQL_CONNECTION = "Server=(localdb)\\mssqllocaldb;Database=ScrumboardTestDb;Trusted_Connection=True;MultipleActiveResultSets=true;";
+        private readonly Mock<ICurrentUserService> _mockCurrentUserService;
+        private readonly Mock<IDateTime> _mockDateTime;
+
+        public ScrumboardDbContext DbContext { get; private set; }
+
+        public DatabaseFixture()
+        {
+            var _currentUserService = "00000000-0000-0000-0000-000000000000";
+            _mockCurrentUserService = new Mock<ICurrentUserService>();
+            _mockCurrentUserService.Setup(m => m.UserId).Returns(_currentUserService);
+
+            _mockDateTime = new Mock<IDateTime>();
+            _mockDateTime.Setup(m => m.Now).Returns(DateTime.Now);          
+        }  
+
+        public IAsyncRepository<T, TId> GetRepository<T, TId>(bool inMemoryDatabase = false) where T : class, Domain.Interfaces.IEntity<TId>
+        {      
+            if (DbContext == null)
+                SetDbContext(inMemoryDatabase);
+
+            return new BaseRepository<T,TId>(DbContext);
+        }
+
+        public static async Task ResetState()
+        {
+            Checkpoint checkpoint = new()
+            {
+                TablesToIgnore = new[] { "__EFMigrationsHistory" }
+            };
+
+            await checkpoint.Reset(DEFAULT_SQL_CONNECTION);
+        }
+
+        private static DbContextOptions<ScrumboardDbContext> CreateNewContextOptions(bool inMemoryDatabase)
+        {
+            DbContextOptionsBuilder<ScrumboardDbContext> builder;
+
+            if (inMemoryDatabase)
+            {
+                // Create a fresh service provider, and therefore a fresh
+                // InMemory database instance.
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                builder = new DbContextOptionsBuilder<ScrumboardDbContext>();
+                builder.UseInMemoryDatabase("ScrumboardTestDb")
+                       .UseInternalServiceProvider(serviceProvider);
+            }
+            else
+            {
+                // Create a fresh service provider, and therefore a fresh
+                // Sql Server database instance.
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkSqlServer()
+                    .BuildServiceProvider();
+
+                builder = new DbContextOptionsBuilder<ScrumboardDbContext>();
+                builder.UseSqlServer(DEFAULT_SQL_CONNECTION)
+                       .UseInternalServiceProvider(serviceProvider);
+            }
+
+            return builder.Options;
+        }
+
+        public void SetDbContext(bool inMemoryDatabase = false)
+        {
+            var options = CreateNewContextOptions(inMemoryDatabase);
+            var operationalStoreOptions = Options.Create(new OperationalStoreOptions());
+
+            DbContext = new ScrumboardDbContext(options, operationalStoreOptions, _mockCurrentUserService.Object, _mockDateTime.Object);
+            DbContext.Database.Migrate();
+        }
+    }
+}

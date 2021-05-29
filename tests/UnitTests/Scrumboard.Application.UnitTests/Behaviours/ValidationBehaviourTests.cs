0eddd1afe81914128b56cf785c94589ad19e96bf
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-05-29 19:58:22 +0200

    Add Validation behaviour unit test

---
+++ tests/UnitTests/Scrumboard.Application.UnitTests/Behaviours/ValidationBehaviourTests.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using Scrumboard.Application.Behaviours;
+using Scrumboard.Application.Exceptions;
+using Scrumboard.Application.Features.Boards.Commands.UpdateBoard;
+using Scrumboard.Application.Interfaces.Persistence;
+using Scrumboard.Application.Profiles;
+using Scrumboard.Application.UnitTests.Mocks;
+using Scrumboard.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Scrumboard.Application.UnitTests.Behaviours
+{
+    public class ValidationBehaviourTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IAsyncRepository<Board, int>> _mockBoardRepository;
+
+        public ValidationBehaviourTests()
+        {
+            _mockBoardRepository = RepositoryMocks.GetBoardRepository();
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("  ")]
+        public async Task Handle_EmptyName_ThrowsAnValidationException(string boardName)
+        {
+            var handler = new UpdateBoardCommandHandler(_mapper, _mockBoardRepository.Object);
+            var updateBoardCommand = new UpdateBoardCommand { Name = boardName, BoardId = 1 };
+            var validationBehavior = new ValidationBehaviour<UpdateBoardCommand, MediatR.Unit>(new List<UpdateBoardCommandValidator>()
+            {
+                new UpdateBoardCommandValidator()
+            });
+
+            // Act
+            Func<Task> action = async () =>
+            {
+                await validationBehavior.Handle(updateBoardCommand, new CancellationToken(), () =>
+                {
+                    return handler.Handle(updateBoardCommand, new CancellationToken());
+                });
+            };
+
+            // Assert
+            await action.Should().ThrowAsync<ValidationException>();
+        }
+    }
+}

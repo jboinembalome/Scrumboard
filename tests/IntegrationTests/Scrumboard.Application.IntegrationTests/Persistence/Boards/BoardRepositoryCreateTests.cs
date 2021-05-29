f090cc49788eb1889ebb5195cfb2f1e2ca8882bb
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-05-29 18:02:39 +0200

    Update variable to private

--- tests/IntegrationTests/Scrumboard.Application.IntegrationTests/Persistence/Boards/BoardRepositoryCreateTests.cs
+++ tests/IntegrationTests/Scrumboard.Application.IntegrationTests/Persistence/Boards/BoardRepositoryCreateTests.cs
@@ -9,7 +9,7 @@ namespace Scrumboard.Application.IntegrationTests.Persistence.Boards
     [Collection("Database collection")]
     public class BoardRepositoryCreateTests : IAsyncLifetime
     {
-        readonly DatabaseFixture _database;
+        private readonly DatabaseFixture _database;
 
         public BoardRepositoryCreateTests(DatabaseFixture database)
         {

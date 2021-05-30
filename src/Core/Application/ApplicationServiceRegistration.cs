8327211db3b34b7028fa15af3c3f17b0009296af
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-05-30 17:06:30 +0200

    Add mediatr pipeline registration

--- src/Core/Application/ApplicationServiceRegistration.cs
+++ src/Core/Application/ApplicationServiceRegistration.cs
@@ -1,5 +1,7 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Scrumboard.Application.Behaviours;
 using System.Reflection;
 
 namespace Scrumboard.Application
@@ -9,7 +11,12 @@ namespace Scrumboard.Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
 
             return services;
         }

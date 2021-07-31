008ce04089a6e07962d8c376cc8daa4781f0d0fb
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-07-31 16:47:57 +0200

    Add WheatherForecast feature

---
+++ src/Core/Application/Features/WeathForecasts/Queries/GetWeatherForecasts/GetWeatherForecastsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Scrumboard.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scrumboard.Application.Features.WeathForecasts.Queries.GetWeatherForecasts
+{
+    public class GetWeatherForecastsQueryHandler : IRequestHandler<GetWeatherForecastsQuery, IEnumerable<WeatherForecast>>
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<IEnumerable<WeatherForecast>> Handle(GetWeatherForecastsQuery request, CancellationToken cancellationToken)
+        {
+            var random = new Random();
+
+            var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = random.Next(-20, 55),
+                Summary = Summaries[random.Next(Summaries.Length)]
+            });
+
+            return Task.FromResult(weatherForecasts);
+        }
+    }
+
+}

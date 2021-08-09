c1c18bb40ebf450c202d17fc15365bdb60bd6187
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-08-09 15:14:19 +0200

    Add code generated by swaggerhub

---
+++ src/UI/Web/ClientApp/src/app/swagger/variables.ts
@@ -0,0 +1,9 @@
+import { InjectionToken } from '@angular/core';
+
+export const BASE_PATH = 'BASE_URL';
+export const COLLECTION_FORMATS = {
+    'csv': ',',
+    'tsv': '   ',
+    'ssv': ' ',
+    'pipes': '|'
+}

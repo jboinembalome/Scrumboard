aa1c4d18a32c716f12a9f56432cb32719ee19b77
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-08-09 15:00:34 +0200

    Update all colors

--- src/Core/Domain/ValueObjects/Colour.cs
+++ src/Core/Domain/ValueObjects/Colour.cs
@@ -15,68 +15,75 @@ namespace Scrumboard.Domain.ValueObjects
         {
         }
 
-        private Colour(string code)
-        {
-            Code = code;
-        }
+        private Colour(string code) => Code = code;
 
         public static Colour From(string code)
         {
             var colour = new Colour { Code = code };
 
             if (!SupportedColours.Contains(colour))
-            {
                 throw new UnsupportedColourException(code);
-            }
 
             return colour;
         }
 
-        public static Colour White => new("#FFFFFF");
+        public static Colour Black => new("bg-black");
 
-        public static Colour Red => new("#FF5733");
+        public static Colour White => new("bg-white");
 
-        public static Colour Orange => new("#FFC300");
+        public static Colour Gray => new("bg-gray-500");
 
-        public static Colour Yellow => new("#FFFF66");
+        public static Colour Teal => new("bg-teal-500");
 
-        public static Colour Green => new("#CCFF99 ");
+        public static Colour Green => new("bg-green-500");
 
-        public static Colour Blue => new("#6666FF");
+        public static Colour Amber => new("bg-amber-500");
 
-        public static Colour Purple => new("#9966CC");
+        public static Colour Orange => new("bg-orange-500");
 
-        public static Colour Grey => new("#999999");
+        public static Colour Violet => new("bg-violet-500");
+
+        public static Colour Purple => new("bg-purple-500");
+
+        public static Colour Pink => new("bg-pink-500");
+
+        public static Colour Rose => new("bg-rose-500");
+
+        public static Colour Indigo => new("bg-indigo-500");
+
+        public static Colour Red => new("bg-red-500");
+
+        public static Colour Yellow => new("bg-yellow-500");
+
+        public static Colour Blue => new("bg-blue-500");  
 
         public string Code { get; private set; }
 
-        public static implicit operator string(Colour colour)
-        {
-            return colour.ToString();
-        }
+        public static implicit operator string(Colour colour) => colour.ToString();
 
-        public static explicit operator Colour(string code)
-        {
-            return From(code);
-        }
+        public static explicit operator Colour(string code) => From(code);
 
-        public override string ToString()
-        {
-            return Code;
-        }
+        public override string ToString() => Code;
 
         protected static IEnumerable<Colour> SupportedColours
         {
             get
             {
+                yield return Black;
                 yield return White;
-                yield return Red;
-                yield return Orange;
-                yield return Yellow;
+                yield return Gray;
+                yield return Teal;
                 yield return Green;
-                yield return Blue;
+                yield return Amber;
+                yield return Orange;
+                yield return Violet;
                 yield return Purple;
-                yield return Grey;
+                yield return Pink;
+                yield return Rose;
+                yield return Indigo;
+                yield return Red;
+                yield return Yellow;
+                yield return Blue;
             }
         }
 

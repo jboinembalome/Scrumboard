6db45a7f20882bb4f16c7ecabaf5363939e12d6b
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-08-15 20:16:08 +0200

    Add setting board component

---
+++ src/UI/Web/ClientApp/src/app/modules/scrumboard/board/setting/setting.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { BoardSettingDto, ColourDto } from 'src/app/swagger';
+
+@Component({
+    selector: 'scrumboard-board-setting',
+    templateUrl: './setting.component.html',
+    styleUrls: ['./setting.component.scss']
+})
+export class SettingComponent {
+    @Input() boardSetting: BoardSettingDto;
+    @Output() boardSettingChange = new EventEmitter<BoardSettingDto>();
+    @Output() closeSettingPanel = new EventEmitter<void>();
+
+    settingForm: FormGroup;
+    ColourDto: ColourDto;
+
+    constructor() {
+    }
+
+    updateColor(color: ColourDto): void {
+        this.boardSetting.colour = color;
+
+        this.boardSettingChange.emit(this.boardSetting);
+    }
+
+    updateCardCoverImage(): void {
+        this.boardSetting.cardCoverImage = !this.boardSetting.cardCoverImage;
+
+        this.boardSettingChange.emit(this.boardSetting);
+    }
+
+    updateSubscribed(): void {
+        this.boardSetting.subscribed = !this.boardSetting.subscribed;
+
+        this.boardSettingChange.emit(this.boardSetting);
+    }
+}
+

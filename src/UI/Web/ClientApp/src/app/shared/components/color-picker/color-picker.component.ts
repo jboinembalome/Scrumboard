7599e2e18b805af7d1f39d145cdda0cde8622282
Jimmy Boinembalome <boinembalome.jimmy@hotmail.fr>
2021-08-15 20:12:10 +0200

    Cleanup shared components

--- src/UI/Web/ClientApp/src/app/shared/components/color-picker/color-picker.component.ts
+++ src/UI/Web/ClientApp/src/app/shared/components/color-picker/color-picker.component.ts
@@ -26,7 +26,7 @@ export class ColorPickerComponent {
             { colour: 'bg-amber-500' },
             { colour: 'bg-green-500' },
             { colour: 'bg-teal-500' },
-            { colour: 'bg-black' },
+            { colour: 'bg-gray-900' },
             { colour: 'bg-gray-500' },
             { colour: 'bg-white' }
         ];
@@ -34,45 +34,8 @@ export class ColorPickerComponent {
     constructor() {
     }
 
-    displayColor(color: ColourDto): string {
-        let strColor: string;
-
-        switch (color.colour) {
-            case "bg-blue-500":
-                strColor = "Blue";
-            case "bg-yellow-500":
-                strColor = "Yellow";
-            case "bg-red-500":
-                strColor = "Red";
-            case "bg-indigo-500":
-                strColor = "Indigo";
-            case "bg-rose-500":
-                strColor = "Rose";
-            case "bg-pink-500":
-                strColor = "Pink";
-            case "bg-purple-500":
-                strColor = "Purple";
-            case "bg-violet-500":
-                strColor = "Violet";
-            case "bg-orange-500":
-                strColor = "Orange";
-            case "bg-amber-500":
-                strColor = "Amber";
-            case "bg-green-500":
-                strColor = "Green";
-            case "bg-teal-500":
-                strColor = "Teal";
-            case "bg-black":
-                strColor = "Black";
-            case "bg-gray-500":
-                strColor = "Gray";
-            case "bg-white":
-                strColor = "White";
-            default:
-                strColor = color.colour;
-
-                this.colorChange.emit(color);
-                return strColor;
-        }
+    updateColor(color: ColourDto): void {
+        this.color = color;
+        this.colorChange.emit(this.color);
     }
 }

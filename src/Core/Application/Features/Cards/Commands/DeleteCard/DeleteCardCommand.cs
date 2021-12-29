1b508f9a72a044f5a72bf9ad873f43faaafc3233
Jimmy Boinembalome <jboinembalome@gmail.com>
2021-12-29 19:29:33 +0100

    Add DeleteCard

---
+++ src/Core/Application/Features/Cards/Commands/DeleteCard/DeleteCardCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Scrumboard.Application.Features.Cards.Commands.DeleteCard
+{
+    public class DeleteCardCommand : IRequest
+    {
+        public int CardId { get; set; }
+    }
+}
---
+++ src/Core/Application/Features/Cards/Commands/DeleteCard/DeleteCardHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using Scrumboard.Application.Exceptions;
+using Scrumboard.Application.Interfaces.Persistence;
+using Scrumboard.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scrumboard.Application.Features.Cards.Commands.DeleteCard
+{
+    public class DeleteCardHandler : IRequestHandler<DeleteCardCommand>
+    {
+        private readonly IAsyncRepository<Card, int> _cardRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteCardHandler(IMapper mapper, IAsyncRepository<Card, int> cardRepository)
+        {
+            _mapper = mapper;
+            _cardRepository = cardRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteCardCommand request, CancellationToken cancellationToken)
+        {
+            var cardToDelete = await _cardRepository.GetByIdAsync(request.CardId, cancellationToken);
+
+            if (cardToDelete == null)
+                throw new NotFoundException(nameof(Card), request.CardId);
+
+            await _cardRepository.DeleteAsync(cardToDelete, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}

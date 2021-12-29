9e325ea43bfc8f776122186a1fb5e5426c783225
Jimmy Boinembalome <jboinembalome@gmail.com>
2021-12-29 19:08:55 +0100

    Add activated route and update board

--- src/UI/Web/ClientApp/src/app/modules/scrumboard/board/listboards/card/card-detail/card-detail.component.ts
+++ src/UI/Web/ClientApp/src/app/modules/scrumboard/board/listboards/card/card-detail/card-detail.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ScrumboardService } from 'src/app/modules/scrumboard/scrumboard.service';
 import { DialogCardComponent } from './dialog-card/dialog-card.component';
 
 
@@ -10,28 +13,42 @@ import { DialogCardComponent } from './dialog-card/dialog-card.component';
   styleUrls: ['./card-detail.component.scss']
 
 })
-export class CardDetailComponent implements OnInit {
+export class CardDetailComponent implements OnInit, OnDestroy {
+
+  private _unsubscribeAll: Subject<any> = new Subject<any>();
 
   constructor(
+    private _scrumboardService: ScrumboardService,
     private _activatedRoute: ActivatedRoute,
     private _matDialog: MatDialog,
     private _router: Router
   ) {
   }
 
-  ngOnInit(): void
-  {
-      // Launch the modal
-      this._matDialog.open(DialogCardComponent, {
-        //disableClose: true, // Maybe use this
-        panelClass: 'custom-dialog-container', 
-      })
-          .afterClosed()
+  ngOnInit(): void {
+    // Launch the modal
+    this._matDialog.open(DialogCardComponent, {
+      //disableClose: true, // Maybe use this
+      panelClass: 'custom-dialog-container',
+      data: {
+        route: this._activatedRoute
+      }
+    })
+      .afterClosed()
+      .subscribe(() => {
+        this._scrumboardService.getBoard(+this._activatedRoute.parent.snapshot.paramMap.get('boardId'))
+          .pipe(takeUntil(this._unsubscribeAll))
           .subscribe(() => {
+            // Go up twice because card routes are setup like this; "card/CARD_ID"
+            this._router.navigate(['./../..'], { relativeTo: this._activatedRoute });
 
-              // Go up twice because card routes are setup like this; "card/CARD_ID"
-              this._router.navigate(['./../..'], {relativeTo: this._activatedRoute});
           });
+      });
   }
 
+  ngOnDestroy() {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next(null);
+    this._unsubscribeAll.complete();
+  }
 }

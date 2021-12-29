41423a1c2ea05c6f8abbba7248c37bdd36572ec8
Jimmy Boinembalome <jboinembalome@gmail.com>
2021-12-29 19:30:10 +0100

    Add label specification

---
+++ src/Core/Application/Features/Labels/Specifications/AllLabelsInBoardSpec.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.Specification;
+using Scrumboard.Domain.Entities;
+using System.Linq;
+
+namespace Scrumboard.Application.Features.Labels.Specifications
+{
+    public class AllLabelsInBoardSpec : Specification<Label>, ISingleResultSpecification
+    {
+        public AllLabelsInBoardSpec(int boardId)
+        {
+            Query.Include(l => l.Cards)
+                .ThenInclude(c => c.ListBoard)
+                .ThenInclude(l => l.Board);
+
+            Query.Where(l => l.Cards.Any(c => c.ListBoard.Board.Id == boardId)); 
+        }
+    }
+}

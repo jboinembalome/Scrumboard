d96d506bf30f47a0854d1da1dd026b8445d52643
Jimmy Boinembalome <jboinembalome@gmail.com>
2021-12-30 10:49:13 +0100

    Add score component

---
+++ src/UI/Web/ClientApp/src/app/modules/memory-game/score/score.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'memory-game-score',
+  templateUrl: './score.component.html',
+  styleUrls: ['./score.component.scss']
+})
+export class ScoreComponent implements OnInit, OnDestroy {
+  @Input() score: number;
+  
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy() {
+    
+  }
+}

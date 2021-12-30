694d39e0adef7f0e4fd7841567f2702e509aa039
Jimmy Boinembalome <jboinembalome@gmail.com>
2021-12-30 10:50:46 +0100

    Add memory game module

---
+++ src/UI/Web/ClientApp/src/app/modules/memory-game/memory-game.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ComponentModule  } from 'src/app/shared/components/component.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CountdownModule } from 'ngx-countdown';
+import { MemoryGameComponent } from './memory-game.component';
+import { CardComponent } from './card/card.component';
+import { ScoreComponent } from './score/score.component';
+import { ResultsComponent } from './results/results.component';
+import { HighScoreInputComponent } from './high-score-input/high-score-input.component';
+
+import { counterRoutes } from './memory-game.routing';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+
+@NgModule({
+    declarations: [
+        MemoryGameComponent,
+        CardComponent,
+        ScoreComponent,
+        ResultsComponent,
+        HighScoreInputComponent
+    ],
+    imports     : [
+        RouterModule.forChild(counterRoutes),
+        ComponentModule,
+        CountdownModule,
+        MaterialModule,
+        SharedModule
+    ]
+})
+export class MemoryGameModule
+{
+}

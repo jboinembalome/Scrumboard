fec5096ee043c4fdc734f126b502d046aef2d84e
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-04-11 14:22:14 +0200

    Upgrade to angular V13

--- src/UI/Web/ClientApp/src/app/modules/scrumboard/board/listboards/card/card-detail/dialog-card/dialog-card.component.ts
+++ src/UI/Web/ClientApp/src/app/modules/scrumboard/board/listboards/card/card-detail/dialog-card/dialog-card.component.ts
@@ -159,7 +159,7 @@ export class DialogCardComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     // Unsubscribe from all subscriptions
-    this._unsubscribeAll.next();
+    this._unsubscribeAll.next(null);
     this._unsubscribeAll.complete();
   }
 

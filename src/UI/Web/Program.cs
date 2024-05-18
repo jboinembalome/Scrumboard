6b6dbdbbd2d2289b4654e9d1fa3c31d34854b381
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-05-18 21:46:30 +0200

    Add small cleanup

--- src/UI/Web/Program.cs
+++ src/UI/Web/Program.cs
@@ -13,7 +13,7 @@ namespace Scrumboard.Web;
 
 public class Program
 {
-    public async static Task Main(string[] args)
+    public static async Task Main(string[] args)
     {
             var host = CreateHostBuilder(args).Build();
 
@@ -49,7 +49,7 @@ public class Program
             await host.RunAsync();
         }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {

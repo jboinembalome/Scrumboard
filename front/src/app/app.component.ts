006332aa433befc5dc0edc931ef1d91bd6f4bcc3
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-06-08 20:53:32 +0200

    Revert "Bootstrap the project using standalone APIs"
    This reverts commit f03a5554329b8b9a6473295a003f3c1c496fa075.

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [RouterOutlet]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';

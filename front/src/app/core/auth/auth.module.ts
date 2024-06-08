d09c45a6f78ea03fb0ef9cc9e9d5aeb0b650e2d5
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-06-08 21:50:40 +0200

    Small cleanup

--- front/src/app/core/auth/auth.module.ts
+++ front/src/app/core/auth/auth.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 import { IdentityService } from './services/identity.service';
 import { LogoutComponent } from './logout/logout.component';
 
 
 @NgModule({
     imports: [
-    HttpClientModule,
     RouterModule.forChild([
         { path: 'login', component: LoginComponent },
         { path: 'logout', component: LogoutComponent }

8a10da6e4e88526da93207e0470a37a4c11ad4ab
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-06-23 20:05:19 +0200

    Add nullable for card description

--- back/Scrumboard.Application/Cards/Dtos/CardDetailDto.cs
+++ back/Scrumboard.Application/Cards/Dtos/CardDetailDto.cs
@@ -9,7 +9,7 @@ public sealed class CardDetailDto
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public bool Suscribed { get; set; }
     public DateTime? DueDate { get; set; }
     public int Position { get; set; }

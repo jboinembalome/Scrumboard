87704c47dca8a96f97522b70a6551bc7a17af4d3
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-06-23 18:32:40 +0200

    Remove select checkbox component

--- front/src/app/shared/components/component.module.ts
+++ front/src/app/shared/components/component.module.ts
@@ -4,7 +4,6 @@ import { ColorPickerComponent } from './color-picker/color-picker.component';
 import { SimpleColorPickerComponent } from './color-picker/simple-color-picker/simple-color-picker.component';
 import { ToggleComponent } from './toggle/toggle.component';
 import { SelectComponent } from './selects/select/select.component';
-import { SelectCheckboxComponent } from './selects/select-checkbox/select-checkbox.component';
 import { SimpleCardComponent } from './cards/simple-card/simple-card.component';
 import { AdherentSelectorComponent } from './adherent-selector/adherent-selector.component';
 
@@ -16,7 +15,6 @@ import { AdherentSelectorComponent } from './adherent-selector/adherent-selector
     SimpleColorPickerComponent,
     ToggleComponent,
     SelectComponent,
-    SelectCheckboxComponent,
     SimpleCardComponent
 ],
     exports: [
@@ -25,7 +23,6 @@ import { AdherentSelectorComponent } from './adherent-selector/adherent-selector
         SimpleColorPickerComponent,
         ToggleComponent,
         SelectComponent,
-        SelectCheckboxComponent,
         SimpleCardComponent
     ]
 })

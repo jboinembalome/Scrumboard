e1d47cdad3854644e34652284261614b8e5c7715
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-06-23 20:05:57 +0200

    Add nullable into domain

--- back/Scrumboard.Domain/Cards/Activities/Activity.cs
+++ back/Scrumboard.Domain/Cards/Activities/Activity.cs
@@ -11,7 +11,7 @@ public sealed class Activity : IAuditableEntity, IEntity<int>
     {
     }
 
-    public Activity(ActivityType activityType, ActivityField activityField, string oldValue, string newValue)
+    public Activity(ActivityType activityType, ActivityField activityField, string? oldValue, string? newValue)
     {
         ActivityType = activityType;
         ActivityField = activityField;
@@ -22,8 +22,8 @@ public sealed class Activity : IAuditableEntity, IEntity<int>
     public int Id { get; set; }
     public ActivityType ActivityType { get; set; }
     public ActivityField ActivityField { get; set; }
-    public string OldValue { get; set; }
-    public string NewValue { get; set; }
+    public string? OldValue { get; set; }
+    public string? NewValue { get; set; }
     public Adherent Adherent { get; set; }
     public Card Card { get; set; }
     public string? CreatedBy { get; set; }
--- back/Scrumboard.Domain/Cards/Card.cs
+++ back/Scrumboard.Domain/Cards/Card.cs
@@ -14,7 +14,7 @@ public sealed class Card : IAuditableEntity, IEntity<int>
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public bool Suscribed { get; set; }
     public DateTime? DueDate { get; set; }
     public int Position { get; set; }

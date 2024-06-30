f86c3ec5ef77c94b248d45f6afc79eaed5236001
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-06-30 19:06:00 +0200

    Update CurrentUserService according to Identity

--- back/Scrumboard.Web/Services/CurrentUserService.cs
+++ back/Scrumboard.Web/Services/CurrentUserService.cs
@@ -3,23 +3,8 @@ using Scrumboard.Infrastructure.Abstractions.Common;
 
 namespace Scrumboard.Web.Services;
 
-internal sealed class CurrentUserService : ICurrentUserService
+internal sealed class CurrentUserService(
+    IHttpContextAccessor httpContextAccessor) : ICurrentUserService
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
-
-    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
-    {
-        _httpContextAccessor = httpContextAccessor;
-    }
-    
-    public Guid UserId
-    {
-        get
-        {
-            var userId = _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
-            return userId is not null 
-                ? Guid.Parse(userId) 
-                : Guid.Empty;
-        }
-    }
+    public string UserId => httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier)!;
 }

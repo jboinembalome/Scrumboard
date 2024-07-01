2634fce0a536621e610726ac150fc631036fe6d7
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-07-01 21:26:07 +0200

    Update AdherentProfile

--- back/Scrumboard.Application/Adherents/AdherentProfile.cs
+++ back/Scrumboard.Application/Adherents/AdherentProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Scrumboard.Application.Adherents.Dtos;
+using Scrumboard.Domain.Adherents;
 using Scrumboard.Infrastructure.Abstractions.Identity;
 
 namespace Scrumboard.Application.Adherents;
@@ -9,7 +10,12 @@ internal sealed class AdherentProfile : Profile
     public AdherentProfile()
     {
         // Read
+        CreateMap<Adherent, AdherentDto>();
+        
         CreateMap<IUser, AdherentDto>()
             .ForMember(dest => dest.HasAvatar, opt => opt.MapFrom(src => src.Avatar.Any()));
+        
+        // Write
+        CreateMap<AdherentDto, Adherent>();
     }
 }

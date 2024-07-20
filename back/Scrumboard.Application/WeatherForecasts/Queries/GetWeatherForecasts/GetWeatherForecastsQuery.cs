41dd91c764baf62fdac5139cd262f181873e6145
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-07-20 15:24:32 +0200

    Remove unused query

--- back/Scrumboard.Application/WeatherForecasts/Queries/GetWeatherForecasts/GetWeatherForecastsQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-using Scrumboard.Domain.Weathers;
-
-namespace Scrumboard.Application.WeatherForecasts.Queries.GetWeatherForecasts;
-
-public sealed class GetWeatherForecastsQuery : IRequest<IEnumerable<WeatherForecast>>
-{
-}

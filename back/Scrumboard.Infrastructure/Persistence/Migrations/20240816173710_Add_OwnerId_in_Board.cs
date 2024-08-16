9edbee42bfcfe0cc7454f35b17b49f0f6a9e87ad
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-08-16 19:37:40 +0200

    Add EF Migration

---
+++ back/Scrumboard.Infrastructure/Persistence/Migrations/20240816173710_Add_OwnerId_in_Board.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Scrumboard.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_OwnerId_in_Board : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Boards",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Boards");
+        }
+    }
+}

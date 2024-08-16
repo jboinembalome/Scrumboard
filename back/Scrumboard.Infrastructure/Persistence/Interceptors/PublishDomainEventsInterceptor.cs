1cef0666ef4f0faea50667315b0cbf1bc68473bf
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-08-16 16:48:51 +0200

    Add comment

--- back/Scrumboard.Infrastructure/Persistence/Interceptors/PublishDomainEventsInterceptor.cs
+++ back/Scrumboard.Infrastructure/Persistence/Interceptors/PublishDomainEventsInterceptor.cs
@@ -9,6 +9,12 @@ internal sealed class PublishDomainEventsInterceptor(
 {
     public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
     {
+        // Dispatch Domain Events collection. 
+        // Choices:
+        // A. Right BEFORE committing data (EF SaveChanges) into the DB will make a single transaction including  
+        // side effects from the domain event handlers which are using the same DbContext with "InstancePerLifetimeScope" or "scoped" lifetime
+        // B. Right AFTER committing data (EF SaveChanges) into the DB will make multiple transactions. 
+        // You will need to handle eventual consistency and compensatory actions in case of failures in any of the Handlers. 
         await PublishDomainEventsAsync(eventData.Context, cancellationToken);
 
         return await base.SavingChangesAsync(eventData, result, cancellationToken);

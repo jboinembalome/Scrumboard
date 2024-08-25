009f670e0686fcfd9d9501c9102303af2f49e247
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-08-25 20:56:49 +0200

    Add functional tests for board

--- back/Scrumboard.Web.FunctionalTests/Api/Boards/BoardsTests.cs
+++ back/Scrumboard.Web.FunctionalTests/Api/Boards/BoardsTests.cs
@@ -1,7 +1,5 @@
 ﻿using System.Net;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Newtonsoft.Json;
 using Scrumboard.Domain.Boards;
 using Scrumboard.Domain.Common;
 using Scrumboard.Web.Api.Boards;
@@ -13,21 +11,6 @@ namespace Scrumboard.Web.FunctionalTests.Api.Boards;
 public sealed class BoardsTests(
     CustomWebApplicationFactory factory) : FunctionalTestsBase(factory)
 {
-    [Theory]
-    [InlineData("/api/boards")]
-    [InlineData("/api/boards/1")]
-    public async Task Get_endpoints_should_return_Unauthorized_when_anonymous_user(string url)
-    {
-        // Arrange
-        var client = _factory.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
-
-        // Act
-        var response = await client.GetAsync(url);
-
-        // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
-    }
-
     [Fact]
     public async Task Get_boards_should_return_Ok()
     {
@@ -36,15 +19,22 @@ public sealed class BoardsTests(
 
         // Act
         var response = await client.GetAsync("/api/boards");
-        response.EnsureSuccessStatusCode();
-
-        var responseString = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<IReadOnlyCollection<BoardDto>>(responseString);
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        result.Should().BeAssignableTo<IEnumerable<BoardDto>>();
-        result.Should().NotBeEmpty();
+    }
+    
+    [Fact]
+    public async Task Get_boards_should_return_Forbidden_when_user_has_no_right()
+    {
+        // Arrange
+        var client = _factory.CreateUserClient(SeededUser.NoRightUser);
+
+        // Act
+        var response = await client.GetAsync("/api/boards");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
     }
 
     [Fact]
@@ -53,25 +43,121 @@ public sealed class BoardsTests(
         // Arrange
         var client = _factory.CreateUserClient(SeededUser.Adherent);
 
-        var board = await Given_a_board();
+        var board = await Given_a_board(SeededUser.Adherent.Id);
         
         // Act
         var response = await client.GetAsync($"/api/boards/{board.Id}");
-        response.EnsureSuccessStatusCode();
-
-        var responseString = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<BoardDto>(responseString);
-
+        
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        result.Should().BeOfType<BoardDto>();
-        result.Should().NotBeNull();
+    }
+    
+    [Fact]
+    public async Task Get_board_by_id_should_return_Forbidden_when_user_has_no_right()
+    {
+        // Arrange
+        var client = _factory.CreateUserClient(SeededUser.NoRightUser);
+        
+        var board = await Given_a_board(SeededUser.NoRightUser.Id);
+
+        // Act
+        var response = await client.GetAsync($"/api/boards/{board.Id}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+    
+    [Fact]
+    public async Task Post_board_should_return_Created()
+    {
+        // Arrange
+        var client = _factory.CreateUserClient(SeededUser.Adherent);
+
+        var payload = BuildPostBoardPayload();
+        
+        // Act
+        var response = await client.PostAsJsonAsync("/api/boards", payload);
+        
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+    
+    [Fact]
+    public async Task Post_board_should_return_Forbidden_when_user_has_no_right()
+    {
+        // Arrange
+        var client = _factory.CreateUserClient(SeededUser.NoRightUser);
+        
+        var payload = BuildPostBoardPayload();
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/boards", payload);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+    
+    [Fact]
+    public async Task Put_board_should_return_Ok()
+    {
+        // Arrange
+        var client = _factory.CreateUserClient(SeededUser.Adherent);
+
+        var board = await Given_a_board(SeededUser.Adherent.Id);
+        
+        var payload = BuildPutBoardPayload(board.Id);
+        
+        // Act
+        var response = await client.PutAsJsonAsync($"/api/boards/{board.Id}", payload);
+        
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async Task Put_board_should_return_Forbidden_when_user_has_no_right()
+    {
+        // Arrange
+        var client = _factory.CreateUserClient(SeededUser.NoRightUser);
+        
+        var board = await Given_a_board(SeededUser.NoRightUser.Id);
+        
+        var payload = BuildPostBoardPayload();
+
+        // Act
+        var response = await client.PutAsJsonAsync($"/api/boards/{board.Id}", payload);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
     }
 
-    private async Task<Board> Given_a_board()
+    private async Task<Board> Given_a_board(OwnerId ownerId)
         => await _factory.AddEntityAsync(new Board(
             name: "New Board",
             isPinned: false,
             boardSetting: new BoardSetting { Colour = Colour.Blue },
-            ownerId: SeededUser.Adherent.Id));
+            ownerId: ownerId));
+    
+    private static BoardCreationDto BuildPostBoardPayload()
+        => new()
+        {
+            Name = "New Board",
+            IsPinned = false,
+            BoardSetting = new BoardSettingCreationDto
+            {
+                Colour = Colour.Blue
+            }
+        };
+    
+    private static BoardEditionDto BuildPutBoardPayload(BoardId boardId)
+        => new()
+        {
+            Id = boardId.Value,
+            Name = "New Board",
+            IsPinned = false,
+            BoardSetting = new BoardSettingEditionDto
+            {
+                Colour = Colour.Blue
+            }
+        };
 }
---
+++ back/Scrumboard.Web.FunctionalTests/Utilities/HttpClientExtensions.cs
@@ -0,0 +1,31 @@
+using System.Text;
+using Scrumboard.Shared.TestHelpers.Serializations;
+
+namespace Scrumboard.Web.FunctionalTests.Utilities;
+
+public static class HttpClientExtensions
+{
+    public static async Task<HttpResponseMessage> PostAsJsonAsync<T>(
+        this HttpClient client, 
+        string requestUrl, 
+        T payload) where T : notnull
+    {
+        var stringContent = new StringContent(
+            SerializationHelper.Serialize(payload),
+            Encoding.UTF8, "application/json");
+        
+        return await client.PostAsync(requestUrl, stringContent);
+    }
+    
+    public static async Task<HttpResponseMessage> PutAsJsonAsync<T>(
+        this HttpClient client, 
+        string requestUrl, 
+        T payload) where T : notnull
+    {
+        var stringContent = new StringContent(
+            SerializationHelper.Serialize(payload),
+            System.Text.Encoding.UTF8, "application/json");
+        
+        return await client.PutAsync(requestUrl, stringContent);
+    }
+}

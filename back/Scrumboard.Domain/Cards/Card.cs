5121420dc5261eadcd731de365572478b8b7a89b
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-08-29 20:09:32 +0200

    Rename private fields for EF Core

--- back/Scrumboard.Domain/Cards/Card.cs
+++ back/Scrumboard.Domain/Cards/Card.cs
@@ -8,58 +8,58 @@ namespace Scrumboard.Domain.Cards;
 
 public sealed class Card : AuditableEntityBase<CardId>
 {
-    private readonly List<CardAssignee> _cardAssignees = [];
-    private readonly List<CardLabel> _cardLabels = [];
+    private readonly List<CardAssignee> _assignees = [];
+    private readonly List<CardLabel> _labels = [];
     
     public string Name { get; set; }
     public string? Description { get; set; }
     public DateTimeOffset? DueDate { get; set; }
     public int Position { get; set; }
     public ListBoardId ListBoardId { get; set; }
-    public IReadOnlyCollection<CardAssignee> Assignees => _cardAssignees.AsReadOnly();
-    public IReadOnlyCollection<CardLabel> Labels => _cardLabels.AsReadOnly();
+    public IReadOnlyCollection<CardAssignee> Assignees => _assignees.AsReadOnly();
+    public IReadOnlyCollection<CardLabel> Labels => _labels.AsReadOnly();
     
     public void AddAssignee(AssigneeId assigneeId)
     {
-        if (_cardAssignees.Any(x => x.CardId == Id && x.AssigneeId == assigneeId))
+        if (_assignees.Any(x => x.CardId == Id && x.AssigneeId == assigneeId))
         {
             return;
         }
         
-        _cardAssignees.Add(new CardAssignee { CardId = Id, AssigneeId = assigneeId });
+        _assignees.Add(new CardAssignee { CardId = Id, AssigneeId = assigneeId });
     }
 
     public void RemoveAssignee(AssigneeId assigneeId)
     {
-        var cardAssignee = _cardAssignees.FirstOrDefault(x => x.CardId == Id && x.AssigneeId == assigneeId);
+        var cardAssignee = _assignees.FirstOrDefault(x => x.CardId == Id && x.AssigneeId == assigneeId);
         
         if (cardAssignee is null)
         {
             return;
         }
         
-        _cardAssignees.Remove(cardAssignee);
+        _assignees.Remove(cardAssignee);
     }
     
     public void AddLabel(LabelId labelId)
     {
-        if (_cardLabels.Any(x => x.CardId == Id && x.LabelId == labelId))
+        if (_labels.Any(x => x.CardId == Id && x.LabelId == labelId))
         {
             return;
         }
         
-        _cardLabels.Add(new CardLabel { CardId = Id, LabelId = labelId });
+        _labels.Add(new CardLabel { CardId = Id, LabelId = labelId });
     }
 
     public void RemoveLabel(LabelId labelId)
     {
-        var cardLabel = _cardLabels.FirstOrDefault(x => x.CardId == Id && x.LabelId == labelId);
+        var cardLabel = _labels.FirstOrDefault(x => x.CardId == Id && x.LabelId == labelId);
         
         if (cardLabel is null)
         {
             return;
         }
         
-        _cardLabels.Remove(cardLabel);
+        _labels.Remove(cardLabel);
     }
 }
--- back/Scrumboard.Domain/Teams/Team.cs
+++ back/Scrumboard.Domain/Teams/Team.cs
@@ -7,7 +7,7 @@ namespace Scrumboard.Domain.Teams;
 
 public sealed class Team : CreatedAtEntityBase<TeamId>
 {
-    private readonly List<TeamMember> _teamMembers = [];
+    private readonly List<TeamMember> _members = [];
 
     public Team() { }
     
@@ -21,21 +21,21 @@ public sealed class Team : CreatedAtEntityBase<TeamId>
 
     public string Name { get; private set; }
     public BoardId BoardId { get; private set; }
-    public IReadOnlyCollection<TeamMember> Members => _teamMembers.AsReadOnly();
+    public IReadOnlyCollection<TeamMember> Members => _members.AsReadOnly();
     
     public void AddMembers(IEnumerable<MemberId> memberIds)
     {
         var memberIdsList = memberIds.ToHashSet();
         
         var membersToAdd = memberIdsList
-            .Where(memberId => !_teamMembers.Exists(tm => tm.MemberId == memberId))
+            .Where(memberId => !_members.Exists(tm => tm.MemberId == memberId))
             .Select(memberId => new TeamMember { TeamId = Id, MemberId = memberId })
             .ToArray();
         
         // Add new members only if there are any
         if (membersToAdd.Length > 0)
         {
-            _teamMembers.AddRange(membersToAdd);
+            _members.AddRange(membersToAdd);
         }
     }
     
@@ -43,14 +43,14 @@ public sealed class Team : CreatedAtEntityBase<TeamId>
     {
         var memberIdsList = memberIds.ToHashSet();
         
-        if (_teamMembers.Count == memberIdsList.Count 
-            && _teamMembers.All(x => memberIdsList.Contains(x.MemberId)))
+        if (_members.Count == memberIdsList.Count 
+            && _members.All(x => memberIdsList.Contains(x.MemberId)))
         {
             return;
         }
 
         // Remove members who are no longer in the list
-        _teamMembers.RemoveAll(x => !memberIdsList.Contains(x.MemberId));
+        _members.RemoveAll(x => !memberIdsList.Contains(x.MemberId));
         
         AddMembers(memberIdsList);
     }

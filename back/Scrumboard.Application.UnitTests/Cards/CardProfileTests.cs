fa1765e17387d2917e584a40632ffcec6ca32067
Jimmy Boinembalome <jboinembalome@gmail.com>
2024-09-15 16:22:13 +0200

    Add CardProfile tests

---
+++ back/Scrumboard.Application.UnitTests/Cards/CardProfileTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture;
+using AutoMapper;
+using FluentAssertions;
+using Scrumboard.Application.Abstractions.Cards;
+using Scrumboard.Application.Cards;
+using Scrumboard.Domain.Cards;
+using Xunit;
+
+namespace Scrumboard.Application.UnitTests.Cards;
+
+public sealed class CardProfileTests
+{
+    private readonly IFixture _fixture;
+    private readonly IMapper _mapper;
+
+    public CardProfileTests()
+    {
+        _fixture = new Fixture();
+
+        var mapperConfiguration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CardProfile>();
+        });
+
+        _mapper = mapperConfiguration.CreateMapper();
+    }
+
+    [Fact]
+    public void Should_map_CardCreation_to_Card()
+    {
+        // Arrange
+        var cardCreation = _fixture.Create<CardCreation>();
+
+        // Act
+        var card = _mapper.Map<Card>(cardCreation);
+
+        // Assert
+        var expectedCard = new Card(
+            name: cardCreation.Name,
+            description: cardCreation.Description,
+            dueDate: cardCreation.DueDate,
+            position: cardCreation.Position,
+            listBoardId: cardCreation.ListBoardId,
+            assigneeIds: cardCreation.AssigneeIds,
+            labelIds: cardCreation.LabelIds);
+
+        card.Should().BeEquivalentTo(expectedCard);
+    }
+}
--- back/Scrumboard.Application/Cards/CardProfile.cs
+++ back/Scrumboard.Application/Cards/CardProfile.cs
@@ -1,8 +1,6 @@
 ﻿using AutoMapper;
 using Scrumboard.Application.Abstractions.Cards;
-using Scrumboard.Application.Abstractions.Teams;
 using Scrumboard.Domain.Cards;
-using Scrumboard.Domain.Teams;
 
 namespace Scrumboard.Application.Cards;
 
@@ -11,11 +9,6 @@ internal sealed class CardProfile : Profile
     public CardProfile()
     {
         // Write
-        CreateMap<CardInputBase, Card>()
-            .ForMember(dest => dest.Assignees, opt => opt.MapFrom(src => src.AssigneeIds))
-            .ForMember(dest => dest.Labels, opt => opt.MapFrom(src => src.LabelIds))
-            .IncludeAllDerived();
-        
         CreateMap<CardCreation, Card>()
             .ConstructUsing(src => new Card(
                 src.Name, 
